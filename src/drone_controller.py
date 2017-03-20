ef5d1a68c771a9b65b155f7f57eb78e431adf924
bapaden <bapaden@mit.edu>
2017-03-20 21:11:19 +0100

    WIP - Smoothed out velocity command to drone

--- src/drone_controller.py
+++ src/drone_controller.py
@@ -12,8 +12,8 @@ import roslib; roslib.load_manifest('ardrone_tutorials')
 import rospy
 
 # Import the messages we're interested in sending and receiving
-from geometry_msgs.msg import Twist  	 # for sending commands to the drone
-from std_msgs.msg import Empty       	 # for land/takeoff/emergency
+from geometry_msgs.msg import Twist    # for sending commands to the drone
+from std_msgs.msg import Empty         # for land/takeoff/emergency
 from ardrone_autonomy.msg import Navdata # for receiving navdata feedback
 
 # An enumeration of Drone Statuses
@@ -25,56 +25,66 @@ COMMAND_PERIOD = 100 #ms
 
 
 class BasicDroneController(object):
-	def __init__(self):
-		# Holds the current drone status
-		self.status = -1
+  def __init__(self):
+    # Holds the current drone status
+    self.status = -1
+    
+    #hacking the controller a bit
+    self.forward = 0
+    self.sideways = 0
+    self.up = 0
+    self.turn = 0
+    
+    # Subscribe to the /ardrone/navdata topic, of message type navdata, and call self.ReceiveNavdata when a message is received
+    self.subNavdata = rospy.Subscriber('/ardrone/navdata',Navdata,self.ReceiveNavdata) 
+    
+    # Allow the controller to publish to the /ardrone/takeoff, land and reset topics
+    self.pubLand    = rospy.Publisher('/ardrone/land',Empty)
+    self.pubTakeoff = rospy.Publisher('/ardrone/takeoff',Empty)
+    self.pubReset   = rospy.Publisher('/ardrone/reset',Empty)
+    
+    # Allow the controller to publish to the /cmd_vel topic and thus control the drone
+    self.pubCommand = rospy.Publisher('/cmd_vel',Twist)
 
-		# Subscribe to the /ardrone/navdata topic, of message type navdata, and call self.ReceiveNavdata when a message is received
-		self.subNavdata = rospy.Subscriber('/ardrone/navdata',Navdata,self.ReceiveNavdata) 
-		
-		# Allow the controller to publish to the /ardrone/takeoff, land and reset topics
-		self.pubLand    = rospy.Publisher('/ardrone/land',Empty)
-		self.pubTakeoff = rospy.Publisher('/ardrone/takeoff',Empty)
-		self.pubReset   = rospy.Publisher('/ardrone/reset',Empty)
-		
-		# Allow the controller to publish to the /cmd_vel topic and thus control the drone
-		self.pubCommand = rospy.Publisher('/cmd_vel',Twist)
+    # Setup regular publishing of control packets
+    self.command = Twist()#Tangent space of SE(3) i think
+    self.commandTimer = rospy.Timer(rospy.Duration(COMMAND_PERIOD/1000.0),self.SendCommand)
 
-		# Setup regular publishing of control packets
-		self.command = Twist()
-		self.commandTimer = rospy.Timer(rospy.Duration(COMMAND_PERIOD/1000.0),self.SendCommand)
+    # Land the drone if we are shutting down
+    rospy.on_shutdown(self.SendLand)
 
-		# Land the drone if we are shutting down
-		rospy.on_shutdown(self.SendLand)
+  def ReceiveNavdata(self,navdata):
+    # Although there is a lot of data in this packet, we're only interested in the state at the moment  
+    self.status = navdata.state
 
-	def ReceiveNavdata(self,navdata):
-		# Although there is a lot of data in this packet, we're only interested in the state at the moment	
-		self.status = navdata.state
+  def SendTakeoff(self):
+    # Send a takeoff message to the ardrone driver
+    # Note we only send a takeoff message if the drone is landed - an unexpected takeoff is not good!
+    if(self.status == DroneStatus.Landed):
+      self.pubTakeoff.publish(Empty())
 
-	def SendTakeoff(self):
-		# Send a takeoff message to the ardrone driver
-		# Note we only send a takeoff message if the drone is landed - an unexpected takeoff is not good!
-		if(self.status == DroneStatus.Landed):
-			self.pubTakeoff.publish(Empty())
+  def SendLand(self):
+    # Send a landing message to the ardrone driver
+    # Note we send this in all states, landing can do no harm
+    self.pubLand.publish(Empty())
 
-	def SendLand(self):
-		# Send a landing message to the ardrone driver
-		# Note we send this in all states, landing can do no harm
-		self.pubLand.publish(Empty())
+  def SendEmergency(self):
+    # Send an emergency (or reset) message to the ardrone driver
+    self.pubReset.publish(Empty())
 
-	def SendEmergency(self):
-		# Send an emergency (or reset) message to the ardrone driver
-		self.pubReset.publish(Empty())
+  def SetCommand(self,roll=0,pitch=0,yaw_velocity=0,z_velocity=0):
+    self.forward  = pitch
+    self.sideways  = roll
+    self.up  = z_velocity
+    self.turn = yaw_velocity
 
-	def SetCommand(self,roll=0,pitch=0,yaw_velocity=0,z_velocity=0):
-		# Called by the main program to set the current command
-		self.command.linear.x  = pitch
-		self.command.linear.y  = roll
-		self.command.linear.z  = z_velocity
-		self.command.angular.z = yaw_velocity
+  def SendCommand(self,event):
+    self.command.linear.x  = 0.95*self.command.linear.x+0.05*self.forward#low-pass
+    self.command.linear.y  = 0.95*self.command.linear.y+0.05*self.sideways#low-pass
+    self.command.linear.z  = self.up
+    self.command.angular.z = self.turn
 
-	def SendCommand(self,event):
-		# The previously set command is then sent out periodically if the drone is flying
-		if self.status == DroneStatus.Flying or self.status == DroneStatus.GotoHover or self.status == DroneStatus.Hovering:
-			self.pubCommand.publish(self.command)
+    # The previously set command is then sent out periodically if the drone is flying
+    if self.status == DroneStatus.Flying or self.status == DroneStatus.GotoHover or self.status == DroneStatus.Hovering:
+      self.pubCommand.publish(self.command)
 
